0d806080eb070427ad195228f8e42acb75ea87d4
sproates <sproates@gmail.com>
2011-03-15 08:50:00 +0000

    Making it work as a servlet

--- src/Trumpet.scala
+++ src/Trumpet.scala
@@ -1,3 +1,8 @@
+import java.io.{
+  FileReader,
+  IOException
+}
+import java.util.Properties;
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.{
   ServletContextHandler,
@@ -5,8 +10,22 @@ import org.eclipse.jetty.servlet.{
 }
 
 object Trumpet extends {
+
+  val propsFile = "trumpet.properties"
+
   def main(args:Array[String]) = {
-    var server = new Server(8080)
+    var port = 8080
+    try {
+      var props = new Properties
+      props.load(new FileReader(propsFile))
+      var parsedPort = Integer.parseInt(props.getProperty("port"))
+      port = parsedPort
+    } catch {
+      case ex:IOException => println("Unable to load " + propsFile)
+      case ex:NumberFormatException => println("port is not a number. Using " + port)
+      case ex => println("Unknown error: " + ex.getMessage)
+    }
+    var server = new Server(port)
     var context = new ServletContextHandler(ServletContextHandler.SESSIONS)
     context.setContextPath("/")
     server.setHandler(context)
